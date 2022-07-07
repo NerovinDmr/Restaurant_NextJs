c2e666a0d573b30bfeb0ac107b92f3d2d96b4c2f
Sergei Firsov <47758071+NerovinDmr@users.noreply.github.com>
2022-07-07 11:56:47 +0300

    Update index.tsx

--- components/Tagline/index.tsx
+++ components/Tagline/index.tsx
@@ -1,11 +1,11 @@
-//@ts-nocheck
+
 import React from "react";
-import Flip from "react-reveal/Flip";
+
 import style from "./styles.module.scss";
 export const Tagline: React.FC = () => {
   return (
     <div className={style.tagline}>
-      <Flip left cascade>
+      
         <div className={style.heading}>
           <h1>
             Delivery of delicious
@@ -14,7 +14,7 @@ export const Tagline: React.FC = () => {
 
           <div className={style.tagline_btn}>haven't tried it yet?</div>
         </div>
-      </Flip>
+      
     </div>
   );
 };
